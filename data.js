1c687c9f665f5751551aa4826ce5de6b362bbb70
Rowel Gabriel <rowelgabrielvmangabat@gmail.com>
2026-06-28 23:02:55 +0800

    add about section and places section

---
+++ data.js
@@ -0,0 +1,85 @@
+var aboutCards = [
+  {
+    icon: "bi bi-people-fill",
+    title: "Population",
+    desc: "Home to over 3 million people and diverse communities."
+  },
+  {
+    icon: "bi bi-chat-dots-fill",
+    title: "Language",
+    desc: "Pangasinense, Ilocano, and Filipino are widely spoken."
+  },
+  {
+    icon: "bi bi-water",
+    title: "Geography",
+    desc: "Known for beaches, plains, rivers, and Hundred Islands."
+  }
+];
+
+var places = [
+  {
+    href: "hundred-islands.html",
+    images: [
+      "images/hi1.jpg",
+      "images/hi2.jpg",
+      "images/hi3.jpg",
+      "images/hi4.jpg",
+      "images/hi5.jpg",
+      "images/hi6.jpg"
+    ],
+    title: "🏝 Hundred Islands",
+    location: "Alaminos, Pangasinan"
+  },
+  {
+    href: "camp-puor.html",
+    images: [
+      "images/cp1.jpg",
+      "images/cp2.jpg",
+      "images/cp3.jpg",
+      "images/cp4.jpg",
+      "images/cp5.jpg",
+      "images/cp6.jpg"
+    ],
+    title: "⛺ Camp Puor",
+    location: "Bolinao, Pangasinan"
+  },
+  {
+    href: "enchanted-cave.html",
+    images: [
+      "images/ec1.jpg",
+      "images/ec2.jpg",
+      "images/ec3.jpg",
+      "images/ec4.jpg",
+      "images/ec5.jpg",
+      "images/ec6.jpg"
+    ],
+    title: "🪨 Enchanted Cave",
+    location: "Bolinao, Pangasinan"
+  },
+  {
+    href: "cape-bolinao-lighthouse.html",
+    images: [
+      "images/cbl1.jpg",
+      "images/cbl2.jpg",
+      "images/cbl3.jpg",
+      "images/cbl4.jpg",
+      "images/cbl5.jpg",
+      "images/cbl6.jpg"
+    ],
+    title: "💡 Cape Bolinao Lighthouse",
+    location: "Bolinao, Pangasinan"
+  },
+  {
+    href: "patar-beach.html",
+    images: [
+      "images/pb1.jpg",
+      "images/pb2.jpg",
+      "images/pb3.jpg",
+      "images/pb4.jpg",
+      "images/pb5.jpg",
+      "images/pb6.jpg"
+    ],
+    title: "🏖 Patar Beach",
+    location: "Bolinao, Pangasinan"
+  }
+];
---
+++ populate.js
@@ -0,0 +1,55 @@
+var aboutCardsRow = document.getElementById('about-cards');
+
+for (var i = 0; i < aboutCards.length; i++) {
+  aboutCardsRow.innerHTML +=
+    `
+      <div class="col-lg-4 col-md-6">
+        <div class="card h-100 shadow border-0 text-center p-4">
+          <i class="${aboutCards[i].icon} display-4"></i>
+          <div class="card-body">
+            <div class="card-title h4">${aboutCards[i].title}</div>
+            <div class="card-text text-muted">
+              ${aboutCards[i].desc}
+            </div>
+          </div>
+        </div>
+      </div>
+    `;
+}
+
+var placesCardsRow = document.getElementById('places-cards');
+
+for (var i = 0; i < places.length; i++) {
+  var id = "carousel" + i;
+  var imagesHTML = "";
+  
+  for (var j = 0; j < places[i].images.length; j++) {
+    imagesHTML += `
+      <div class="carousel-item ${j === 0 ? "active" : ""}">
+        <img src="${places[i].images[j]}" class="d-block w-100" alt="">
+      </div>
+    `;
+  }
+
+  placesCardsRow.innerHTML += `
+    <div class="col-lg-4 col-md-6">
+      <div class="card h-100 rounded-4 shadow border-0">
+        <div id="${id}" class="carousel slide">
+          <div class="carousel-inner">
+            ${imagesHTML}
+          </div>
+          <button class="carousel-control-prev" type="button" data-bs-target="#${id}" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon"></span>
+          </button>
+          <button class="carousel-control-next" type="button" data-bs-target="#${id}" data-bs-slide="next">
+            <span class="carousel-control-next-icon"></span>
+          </button>
+        </div>
+        <div class="card-body text-center">
+          <div class="card-title fw-bold h4">${places[i].title}</div>
+          <div class="card-text">${places[i].location}</div>
+        </div>
+      </div>
+    </div>
+  `;
+}
